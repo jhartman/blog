c466ba8200c71ed337d4c48c94825ba85c01b7bd
Joshua Hartman <jhartman@linkedin.com>
2012-01-11 18:14:29 -0800

    Css should come before Javascript for performance, fix caching in the GwtRequestFilter.

--- server/src/josh/server/GwtRequestFilter.scala
+++ server/src/josh/server/GwtRequestFilter.scala
@@ -14,15 +14,18 @@ class GwtRequestFilter extends Filter {
     val httpRequest = request.asInstanceOf[HttpServletRequest]
     val httpResponse = response.asInstanceOf[HttpServletResponse]
     val uri = httpRequest.getRequestURI
+
+    val now = System.currentTimeMillis()
     if (uri.contains("nocache")) {
-      val now = System.currentTimeMillis()
       httpResponse.setDateHeader("Date", now)
       httpResponse.setDateHeader("Expires", now)
       httpResponse.setHeader("Pragma", "no-cache")
       httpResponse.setHeader("Cache-control", "no-cache,no-store,must-revalidate")
+    } else if(uri.contains("cache")) {
+      httpResponse.setDateHeader("Expires", now + 30L * 24L * 60L * 60L * 1000L)
+      httpResponse.addHeader("Cache-control", "max-age=108000")
     } else {
-      logger.info("Setting cache control for " + uri)
-//      httpResponse.setHeader("Cache-control", "max-age=31536000")
+      httpResponse.setDateHeader("Expires", now + 60L * 60L * 1000L)
       httpResponse.addHeader("Cache-control", "max-age=3600")
     }
     filterChain.doFilter(httpRequest, httpResponse)
